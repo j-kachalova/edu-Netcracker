59e68fef5b3961d6200630c359ffc651a7ce04a1
j-kachalova <j-kachalova@yandex.ru>
2022-01-26 22:14:26 +0400

    Squashed commit of the following:
    commit 485476bab4958f9bb864653f014918eab2f9f233
    Author: j-kachalova <j-kachalova@yandex.ru>
    Date:   Wed Jan 26 22:01:28 2022 +0400
    
        add password encoder

--- NC/src/main/java/org/example/NC/controller/RegistrationController.java
+++ NC/src/main/java/org/example/NC/controller/RegistrationController.java
@@ -3,37 +3,48 @@ package org.example.NC.controller;
 import org.example.NC.domain.Human;
 import org.example.NC.domain.Role;
 import org.example.NC.repos.UserRepo;
+import org.example.NC.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
 import java.util.Map;
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
     @Autowired
     private UserRepo userRepo;
-
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(Human user, Map<String, Object> model) {
+    public String addUser(@Valid Human user, Model model) {
         Human userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if (userFromDb != null) {
+       /* if (userFromDb != null) {
             model.put("message", "User exists!");
             return "registration";
+        }*/
+        if (!userService.addUser(user)) {
+            //model.addAttribute("usernameError", "User exists!");
+            return "registration";
         }
-
-        user.setActive(true);
+        /*user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
-
+*/
         return "redirect:/login";
     }
 }
--- NC/src/main/java/org/example/NC/service/UserService.java
+++ NC/src/main/java/org/example/NC/service/UserService.java
@@ -1,19 +1,44 @@
 package org.example.NC.service;
 
+import org.example.NC.domain.Human;
+import org.example.NC.domain.Role;
 import org.example.NC.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
+    @Lazy
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         return userRepo.findByUsername(username);
+    }
+    public boolean addUser(Human user) {
+        Human userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepo.save(user);
+
+        return true;
     }
 }
